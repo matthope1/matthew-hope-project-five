aa47bd52462445c500eef24d9d8a04b01677f82a
Matthew <matt-hope@hotmail.com>
2020-09-27 10:49:39 -0400

    added logo and started styling header section

--- keeb-store/src/Header.js
+++ keeb-store/src/Header.js
@@ -5,14 +5,23 @@ class Header extends Component {
         return (
             <div>
                 <nav>
-                    <div className="wrapper">
-
+                    <div className="nav-bar wrapper">
+                        <div class="logo">
+                            <img class="img" src="http://matthopecodes.com/project-five-assets/keebStoreLogo.png" alt="logo"/>
+                        </div>
+                        <ul className="nav-bar__links">
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Store</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                        <div className="nav-bar__cart-icon">
+                            <i class="fas fa-shopping-cart"></i> 
+                        </div> 
                     </div>
                 </nav>
             </div>
         )
     }
 }
-
 
 export default  Header;

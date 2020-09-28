f9bc2727f0dd4b4f113a3be531f716e32bc35b79
Matthew <matt-hope@hotmail.com>
2020-09-28 16:40:22 -0400

    added more to product prop

--- keeb-store/src/Product.js
+++ keeb-store/src/Product.js
@@ -4,9 +4,14 @@ class Product extends Component {
 
     render() {
 
+        const  name  = this.props.name;
+        const { price, type, url } = this.props.productInfo;
+
         return (
             <div>
-                <h1>{ this.props.name }</h1>    
+                <h1>{ name} { price } </h1>    
+                <img src={url} alt=""/>
+                {/* <h2>{ price } </h2> */}
             </div>
         )
     }

03f0ed9ddd13a6483eca63d08a862920cb7ffaaf
Matthew <matt-hope@hotmail.com>
2020-10-01 07:39:33 -0400

    Updates

---
+++ precache-manifest.291d829c3807ac083fab78230bf7ebc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6735c54b70bd3c1391c9",
+    "url": "/matthew-hope-project-five/static/css/main.f155f0c8.chunk.css"
+  },
+  {
+    "revision": "6735c54b70bd3c1391c9",
+    "url": "/matthew-hope-project-five/static/js/main.7c2eeb82.chunk.js"
+  },
+  {
+    "revision": "9c6d5c358530d600df40",
+    "url": "/matthew-hope-project-five/static/js/runtime~main.8d8f4943.js"
+  },
+  {
+    "revision": "f70f1e147b4ed8aca08d",
+    "url": "/matthew-hope-project-five/static/js/2.320e246e.chunk.js"
+  },
+  {
+    "revision": "715440dd58b10c6a849194c6be34f5c0",
+    "url": "/matthew-hope-project-five/index.html"
+  }
+];
